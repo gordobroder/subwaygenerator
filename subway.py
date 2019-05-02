89447336da9d04d969e35bb1adc89ccbdccf97b1
Gustavo Schmidt <gustavo.schmidt@SABIAX.LOCAL>
2019-05-02 09:29:29 -0300

    fixed requirements.txt

--- subway.py
+++ subway.py
@@ -3,17 +3,17 @@ import random
 
 app = Flask(__name__)
 
+#this is the list of all the ingredients of the subway
+bread = ["9 Grãos", "9 Grãos com aveia e mel", "Italiano", "Parmesão com oregano", "Tres queijos"]
+size = ["30cm", "15cm"]
+fill = ["Atum", "Beef Bacon Chipotle", "Beef Barbecue Bacon", "Carne Supreme", "BMT", "Frango", "Frango Defumado",
+            "Frango Empanado", "Frango Ranch", "Frango Teriyaki", "Steak Cheddar Cremoso", "Vegetariano"]
+cheese = ["Cheddar", "Prato", "Provolone"]
+sauce = ["Mostrada e Mel", "Cebola Agridoce", "Barbecue", "Parmesão", "Chipotle", "Mostarda", "Maionese"]
+
 #this is the function and lists of ingredients, it will also randomize the itens to generate a new sub.
 @app.route("/subway/random/")
 def rand():
-    #this is the list of all the ingredients of the subway
-    bread = ["9 Grãos", "9 Grãos com aveia e mel", "Italiano", "Parmesão com oregano", "Tres queijos"]
-    size = ["30cm", "15cm"]
-    fill = ["Atum", "Beef Bacon Chipotle", "Beef Barbecue Bacon", "Carne Supreme", "BMT", "Frango", "Frango Defumado",
-            "Frango Empanado", "Frango Ranch", "Frango Teriyaki", "Steak Cheddar Cremoso", "Vegetariano"]
-    cheese = ["Cheddar", "Prato", "Provolone"]
-    sauce = ["Mostrada e Mel", "Cebola Agridoce", "Barbecue", "Parmesão", "Chipotle", "Mostarda", "Maionese"]
-
 
     #the code bellow will select a random ingredient of the list above
     rbread = random.choice(bread)
@@ -27,7 +27,7 @@ def rand():
 #this route sets the homepage, without randomizing anything, the users must click the buttom to generate a subway
 @app.route("/subway/")
 def subway():
-    return render_template("subway.html")
+    return render_template("subway.html", bread=bread, size=size, fill=fill, cheese=cheese, sauce=sauce)
 
 if __name__ == "__main__":
     app.run()
